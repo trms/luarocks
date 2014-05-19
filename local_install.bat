4f4a61ee8bf7765130d381120fc0ee11c400b553
Andrew Starks <andrew.starks@trms.com>
2014-05-19 16:35:45 -0500

    wip

--- local_install.bat
+++ local_install.bat
@@ -1,19 +1,16 @@
-
-set CURDIR=%CD%
-pushd %cd%\..\
-set PREFIX=%cd%\x64\Debug
-pushd %CURDIR%
+pushd %cd%
+set PREFIX=%cd%\Debug
 set LV=5.3
 set LUA=%PREFIX%
 set LUA_INC=%PREFIX%\include
 set LUA_LIB=%PREFIX%
-set LUA_BIN=%PREFIX%
+set LUA_BIN=%PREFIX%\rocks
 set P=%PREFIX%\luarocks
 set P_PATH= %P%\2.1
 set LR_TREE=%PREFIX%
 
 
-
-
-run install /LV 5.3 /F /BIN "%LUA_BIN%" /LUA "%LUA%" /P "%P%" /INC "%LUA_INC%" /LIB "%LUA_LIB%" /TREE "%LR_TREE%" /NOREG
 set PATH=%PATH%;%P_PATH%;%PREFIX%
+
+run 
+install /LV 5.3 /F /CONFIG "%P%" /FORCECONFIG /BIN "%LUA_BIN%" /LUA "%LUA%" /P "%P%" /INC "%LUA_INC%" /LIB "%LUA_LIB%" /TREE "%LR_TREE%" /NOREG
